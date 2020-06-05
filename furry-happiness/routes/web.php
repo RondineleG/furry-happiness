c522bfa2e4f0d0fb79c34eb24c2de8961a1691f5
rondineleg@edu.univali.br <rondineleg@edu.univali.br>
2020-06-05 12:26:58 -0300

    Criando primeiras rotas

--- furry-happiness/routes/web.php
+++ furry-happiness/routes/web.php
@@ -2,16 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/contato', function (){
+    return view('site.Contact');
+});
+
+Route::get('/hey', function (){
+    return ('Olha Mundo');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
