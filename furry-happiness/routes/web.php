5af58840fab411945c600cfc157888ff01dedfa9
rondineleg@edu.univali.br <rondineleg@edu.univali.br>
2020-06-05 12:49:34 -0300

    Rotas com paramentros e paramentros opcionais

--- furry-happiness/routes/web.php
+++ furry-happiness/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/produtos/{idProduct?}', function($idProduct = 'Nada encontrado!!'){
+    return"Produtos : {$idProduct}";
+});
+
+
+Route::get('/categoria/{flag}/posts', function($flag){
+
+    return "Posts da Produtos da categoria : {$flag}";
+});
+
 Route::any('/any', function() {
     return "any";
 });
